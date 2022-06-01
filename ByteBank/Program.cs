00eb4965ed9b94ad1c792868abd2c4f4a85d876a
Amanda Luiza <77518236+AmandaLuiza@users.noreply.github.com>
2022-06-01 14:52:21 -0300

    Add files via upload

---
+++ ByteBank/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //pagina?argumentos
+            //012345678
+
+            string url = "paginas?argumentos";
+
+           //string temporaria = url + "sufixo";
+            //url = temporaria;
+
+            //url += "sufixo";
+
+            Console.WriteLine(url);
+            string argumentos  = url.Substring(8);
+            Console.WriteLine(argumentos);
+
+
+            Console.ReadLine();
+        }
+    }
+}
